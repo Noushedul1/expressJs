26fecf8c48c98d5b4421843734bf8d0b50725fda
Noushedul1 <noushedakib@gmail.com>
2021-11-03 11:02:20 +0600

    first express js app

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+app = express();
+app.get('/', function(req,res){
+    res.send('hello world');
+});
+app.listen(8000, function(){
+    console.log('server run success');
+});
